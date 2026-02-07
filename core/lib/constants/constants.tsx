eb62f11023f01bd463d1e7c4ed353d995556f440
carlhoro <chrodriguez.bmdc@gmail.com>
2026-02-07 16:49:27 -0500

    Constants file created

---
+++ core/lib/constants/constants.tsx
@@ -0,0 +1,16 @@
+export const HERO_CONTENT = {
+    title: "Ing. Carlos Hoyos",
+    lead: "Mi lead",
+    statement:  "Declaracion",
+    closing: "Frase de cierre"
+}as const 
+
+
+export const HERO_SECTIONS = {
+    Position: "Posición",
+    InternetOfThings: "IoT",
+    DigiGov: "Gobierno digital",
+    knowledge: "Conocimiento",
+    Reference: "Trayectoria",
+    Contact: "Contacto"
+}as const

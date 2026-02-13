2285b675171cee0c9721302e2c7677986b03591e
carlhoro <chrodriguez.bmdc@gmail.com>
2026-02-13 15:00:28 -0500

    component return to hero created outside of section

--- components/Knowledge/knowledge.tsx
+++ components/Knowledge/knowledge.tsx
@@ -1,4 +1,5 @@
 import { KNOWLEDGE_SECTION } from "@/core/lib/constants/constants";
+import { ReturnToHeroButton } from "../layout/returnHeroButton";
 
 export function Knowledge() {
     return (
@@ -29,10 +30,7 @@ export function Knowledge() {
                     </li>
                     <p className="mb-4 font-light">{KNOWLEDGE_SECTION.captionOpenning}</p>
                     <p className="mb-4 font-medium">{KNOWLEDGE_SECTION.captionClosing}</p>
-                    <a href="#hero" className="inline-flex items-center font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700">
-                        Volver a Hero
-                        <svg className="ml-1 w-6 h-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
-                    </a>
+                    <ReturnToHeroButton/>
                 </div>
             </div>
         </section>
